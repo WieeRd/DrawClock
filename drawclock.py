6014f67fa111440420090e93c8326dfc536d55ba
WieeRd_laptop <wowluke@naver.com>
2021-05-14 19:25:24 +0900

    Didn't know those nails were called 'hands'

--- drawclock.py
+++ drawclock.py
@@ -2,10 +2,10 @@
 from PIL import Image
 
 class DrawClock:
-    def __init__(self, frame: Image.Image, h_pin: Image.Image, m_pin: Image.Image):
+    def __init__(self, frame: Image.Image, h_hand: Image.Image, m_hand: Image.Image):
         self.frame = frame
-        self.h_pin = h_pin
-        self.m_pin = m_pin
+        self.h_hand = h_hand
+        self.m_hand = m_hand
 
     def draw(self, hour: int, minute: int) -> Image.Image:
         base = self.frame.copy()
@@ -13,19 +13,19 @@ class DrawClock:
         angle_h = 360 - hour*30 - minute//2
         angle_m = 360 - minute*6
 
-        h_pin = self.h_pin.rotate(angle_h)
-        m_pin = self.m_pin.rotate(angle_m)
+        h_hand = self.h_hand.rotate(angle_h)
+        m_hand = self.m_hand.rotate(angle_m)
 
-        base.paste(h_pin, (0,0), h_pin.convert('RGBA'))
-        base.paste(m_pin, (0,0), m_pin.convert('RGBA'))
+        base.paste(h_hand, (0,0), h_hand.convert('RGBA'))
+        base.paste(m_hand, (0,0), m_hand.convert('RGBA'))
 
         return base
 
 if __name__=="__main__":
     frame = Image.open("./example/frame.png")
-    h_pin = Image.open("./example/h_pin.png")
-    m_pin = Image.open("./example/m_pin.png")
-    dc = DrawClock(frame, h_pin, m_pin)
+    h_hand = Image.open("./example/h_hand.png")
+    m_hand = Image.open("./example/m_hand.png")
+    dc = DrawClock(frame, h_hand, m_hand)
 
     while True:
         get = input("hh:mm = ").split(sep=':')
